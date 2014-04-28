7042ff195553ec2e964bf6adb4b2468b39ffd82e
pkaczor <pawel.kaczor@consileon.pl>
2014-04-28 22:03:11 +0200

    remove redundant implicit view

--- src/main/scala/ddd/support/domain/Addressable.scala
+++ src/main/scala/ddd/support/domain/Addressable.scala
@@ -3,24 +3,9 @@ package ddd.support.domain
 import ddd.support.domain.Addressable.AddressResolver
 
 object Addressable {
-
-  type AddressResolver = PartialFunction[Any, String]
-
-  /**
-   * Implicit view.
-   *
-   * getAddress method will be "added" to any Addressable.
-   *
-   * In fact this method return object of anonymous class that provides method getAddress.
-   * Thus getAddress method will not be called on Addressable but on newly created object
-   * returned by this method.
-   */
-  implicit def addAddressable[T](implicit addressable: Addressable[T]) =
-    new {
-      def addressResolver = addressable.addressResolver
-    }
-
-
+  type Command = Any
+  type AggregateRootIdentifier = String
+  type AddressResolver = PartialFunction[Command, AggregateRootIdentifier]
 }
 
 trait Addressable[T] {
